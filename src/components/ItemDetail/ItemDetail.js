2b6ad060c99ce91a1dba58358710faf0b4096829
khadizaliza <khadizaakter19836@gmail.com>
2022-05-08 22:18:58 +0600

    chectout-create

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 const ItemDetail = () => {
     const {itemId} = useParams();
+    const [item, setItem] = useState({});
+
+    useEffect( () =>{
+        const url = `http://localhost:5000/items/${itemId}`;
+        fetch(url)
+        .then(res=> res.json())
+        .then(data => setItem(data));
+    }, [])
     return (
         <div>
-            <h2>wellcome: {itemId}</h2>
+            <h2>wellcome to detail: {item.name}</h2>
+            <div>
+                <Link to="/checkout">
+                    <button className='btn btn-primary'>Checkout</button>
+                </Link>
+            </div>
         </div>
     );
 };
